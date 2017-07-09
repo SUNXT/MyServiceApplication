3b168653d0342ee2cd6a2de5e33ca61085d9646e
SUNxuedian <sunxt16@qq.com>
2017-07-09 23:14:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sun/myserviceapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sun.myserviceapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public class MessageReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
+    }
+}
